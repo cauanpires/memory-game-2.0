b23fead24e23b5ffc559600789a19a0ce16f6fde
cauanpires <110138401+cauanpires@users.noreply.github.com>
2023-11-10 17:40:10 -0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,101 @@
+const grid = document.querySelector('.grid');
+
+const personagens = [
+    'andrey',
+    'cauã',
+    'carlos',
+    'la coca',
+];
+
+const createElement = (tag, className)=>{
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+let firstcard = '';
+let secundcard= '';
+
+const checkendgame = ()=> {
+    const disabledcards = document.querySelectorAll('.disabled-card');
+
+    if (disabledcards.length == 8){
+        alert('Parabens meu chapa você tem uma mente superior aos demais!');
+    }
+}
+
+
+
+const checkcards =() => {
+    const firstperso = firstcard.getAttribute('data-personagem');
+    const secundperso = secundcard.getAttribute('data-personagem');
+
+    if(firstperso == secundperso) {
+
+        firstcard.firstChild.classList.add('disabled-card');
+        secundcard.firstChild.classList.add('disabled-card');
+
+        firstcard = '';
+        secundcard = '';
+
+        checkendgame();
+
+    }else {
+        setTimeout(() => {
+            firstcard.classList.remove('reveal-card');
+            secundcard.classList.remove('reveal-card');
+
+            firstcard = '';
+            secundcard = '';
+        
+        }, 500)
+
+    
+    }
+}
+
+const revealcard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal-card')){
+        return;
+    }
+    if(firstcard == ''){
+        target.parentNode.classList.add('reveal-card');
+        firstcard = target.parentNode;
+    }else if(secundcard == ''){
+        target.parentNode.classList.add('reveal-card');
+        secundcard = target.parentNode;
+
+        checkcards()
+    }
+}
+
+const createCard = (personagem) =>{
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../imagens/${personagem}.jpeg')` ;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealcard);
+    card.setAttribute('data-personagem', personagem);
+
+    return card;
+}
+
+const loadGame =  () =>{
+
+    const duplicatepersonagens = [ ... personagens, ... personagens];
+
+    const embraralhar = duplicatepersonagens.sort(() => Math.random() -0.5);
+
+    embraralhar.forEach((personagem) => {
+        const  card = createCard(personagem);
+        grid.appendChild(card)
+    });
+}
+
+loadGame();
